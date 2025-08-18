dcdbb9ebcf69baa6ab7424a5221a473edf48551d
sparkdv <lunga.malinga@sparkschools.co.za>
2025-08-18 18:32:38 +0200

    init

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using SigniFlowMiddlewareLibrary;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first nmae?");
+            string ? name = Console.ReadLine();
+
+            Console.WriteLine("What is your surname?");
+            string ? surname = Console.ReadLine();
+
+            string fullnmae = PersonProcessor.JoinName(name, surname);
+            Console.Write(fullnmae);
+
+        }
+    }
+}
---
+++ SigniFlowMiddlewareLibrary/PersonProcessor.cs
@@ -0,0 +1,10 @@
+﻿namespace SigniFlowMiddlewareLibrary;
+
+public class PersonProcessor
+{
+    public static string JoinName(string name, string surname)
+    {
+        return $"{name} {surname}";
+    }
+
+}
